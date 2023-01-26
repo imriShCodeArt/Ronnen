84d7196a90127d2b0d1e56b10241bf016be01cdc
imriSh <59433290+imriShCodeArt@users.noreply.github.com>
2023-01-26 13:49:40 +0200

    Update account.tsx

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,8 +1,14 @@
+import Typography from '@mui/material/Typography'
 import PrimaryLayout from 'shell/containers/PrimaryLayout/PrimaryLayout'
 import { NextPageWithLayout } from './page'
 
 const Account: NextPageWithLayout = () => {
-  return <></>
+  return (
+    <>
+      <Typography variant="h1">חשבון</Typography>
+      <Typography variant="h3">ברוך הבא שם משתמש</Typography>
+    </>
+  )
 }
 
 export default Account
